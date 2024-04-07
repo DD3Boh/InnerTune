dc19295fa0732400c7aef5bc23283fddeb7f8ba2
Davide Garberi <dade.garberi@gmail.com>
2024-04-07 14:06:06 +0200

    innertune: Show saved albums in the Account page

--- app/src/main/java/com/zionhuang/music/viewmodels/AccountViewModel.kt
+++ app/src/main/java/com/zionhuang/music/viewmodels/AccountViewModel.kt
@@ -3,6 +3,7 @@ package com.zionhuang.music.viewmodels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.zionhuang.innertube.YouTube
+import com.zionhuang.innertube.models.AlbumItem
 import com.zionhuang.innertube.models.PlaylistItem
 import com.zionhuang.music.utils.reportException
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -13,11 +14,17 @@ import javax.inject.Inject
 @HiltViewModel
 class AccountViewModel @Inject constructor() : ViewModel() {
     val playlists = MutableStateFlow<List<PlaylistItem>?>(null)
+    val albums = MutableStateFlow<List<AlbumItem>?>(null)
 
     init {
         viewModelScope.launch {
             YouTube.likedPlaylists().onSuccess {
                 playlists.value = it
+            }.onFailure {
+                reportException(it)
+            }
+            YouTube.libraryAlbums().onSuccess {
+                albums.value = it
             }.onFailure {
                 reportException(it)
             }
